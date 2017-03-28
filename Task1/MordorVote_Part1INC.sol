c9767ffb19f8ec2b98245ca4e94a8edafa2e54bc
Abhinav Kalra <abhinavkalra@outlook.com>
2017-03-28 18:11:30 -0300

    Update MordorVote_Part1INC.sol

--- Task1/MordorVote_Part1INC.sol
+++ Task1/MordorVote_Part1INC.sol
@@ -21,24 +21,53 @@ contract MordorVote {
     function CreateUser(string fname, string lname, string dob) returns(string)
     {
         voterid++;
-    /* insert code here*/
+        // code here
         
     }
     function GetVoterId(string lname, string dob) constant returns(uint)
     {
-    /* insert code here*/
+    for ( uint i=0; i < ufull.length; i++)
+    {
+        if ((sha3(ufull[i].lastname) == sha3(lname)) && (sha3(ufull[i].dob) == sha3(dob)))
+        {
+            // code here
+            
+        }
+    }
+    return 0;
         
     }
     function Vote(uint vid, uint vote) returns ( string)
     {
-        /* insert code here */
-    return "User not found";
+        for ( uint i = 0; i < ufull.length; i++)
+        {
+            if (vid == ufull[i].vid)
+            {
+                if(vote ==1)
+                {
+                    // code here
+                }
+                if(vote ==2)
+                {
+                    // code here
+                }
+            }
+            
+        }
+        return "User not found";
         
     }
     function GetVoteCount ( uint cannum) returns ( uint)
     {
-    
-
+        if( cannum == 1)
+        {
+            // code here
+        }
+        if ( cannum ==2 )
+        {
+            // code here
+        }
+    return 0;
 
     }
 
